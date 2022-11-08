040f4bbe1f5290a9510fd4ca619e4d2a36ca2b54
maghnus <g00377514@atu.ie>
2022-11-08 17:08:17 +0000

    added thepassanger class

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,8 +1,18 @@
 package ie.atu;
 
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("=============================================================" + "\nPlease correctly fill out the below questions: \n\n" + "1. Title\t(Mr, Mrs or Ms)\n" + "2. Name\t(Minimum of 3 characters) \n" + "3. ID\t(Minimum of 10 characters)\n" + "4. Phone number\t(Minimum of 7 digits)\n" + "5. Age\t(Under 16 too young to fly)\n" + "=============================================================");
+
+    public static void main(String args[]){
+        
+        System.out.println("=============================================================" +
+            "\nPlease correctly fill out the below questions: \n\n" +
+            "1. Title\t(Mr, Mrs or Ms)\n" +
+            "2. Name\t(Minimum of 3 characters) \n" +
+            "3. ID\t(Minimum of 10 characters)\n" +
+            "4. Phone number\t(Minimum of 7 digits)\n" +
+            "5. Age\t(Under 16 too young to fly)\n" +
+            "=============================================================");
+
+
     }
 }
---
+++ src/main/java/ie/atu/ThePassanger2022.java
@@ -0,0 +1,17 @@
+package ie.atu;
+
+public class ThePassanger2022 {
+
+    public ThePassanger2022()
+    {
+        String usr_title;
+        String usr_name;
+        String usr_id;
+        String usr_phone_num;
+        int usr_age;
+
+    }
+
+
+
+}
